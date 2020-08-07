aad75b1ff1398778e2136399dfabc177047995c4
Francisco Diosdado <drz.y2k@gmail.com>
2020-08-06 23:13:01 -0500

    Agregado bootstrap-vue e interfaz de tareas
    Ya agrega y carga todas las tareas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hello">
+    <div class="row">
+      <div class="col">
+        <!-- textp {{ allTasksList }} -->
+        <b-form-textarea
+          id="text"
+          v-model="taskDescription"
+          placeholder="Enter something..."
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea>
+        <b-button @click="addTask(taskDescription)">Aceptar</b-button>
+      </div>
+      <div class="col" id="tasksList">
+        <b-card v-for="(task,index) in allTasksList" :key="task.id">
+          <b-card-text>
+            {{task.title}} ide:
+            {{task.id}}
+          </b-card-text>
+          <b-button @click="editTask(task,index)" variant="primary" class="mr-2">Editar</b-button>
+          <b-button @click="deleteTask(index)" variant="danger">Eliminar</b-button>
+        </b-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // eslint-disable-next-line
 import { ipcRenderer } from 'electron';
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      taskDescription: 'Tarea',
+      allTasksList: [],
+      allTasksListCounter: 0,
+      taskEditing: false,
+      task2: '',
+    };
   },
+  name: 'Home',
   mounted() {
     ipcRenderer.send('get-all-tasks');
     // ipcRenderer.send('create-task', {
     //   title: 'test3',
     // });
   },
+  methods: {
+    addTask(task) {
+      if (!this.taskEditing) {
+        ipcRenderer.send('create-task', {
+          title: task,
+        });
+        // this.allTasksList.push({
+        //   title: task,
+        //   id: this.allTasksListCounter += 1,
+        // });
+      } else {
+        this.allTasksList[this.task2.indice].title = this.taskDescription;
+        this.taskEditing = false;
+        this.task2 = '';
+      }
+      ipcRenderer.send('get-all-tasks');
+    },
+    editTask(task, index) {
+      this.taskEditing = true;
+      this.taskDescription = task.title;
+      this.task2 = task;
+      this.task2.indice = index;
+    },
+    deleteTask(index) {
+      this.allTasksList.splice(index, 1);
+    },
+  },
   created() {
     ipcRenderer.on('get-all-tasks', (e, args) => {
       console.log('get-all-users', args);
+      this.allTasksList = args;
     });
     ipcRenderer.on('create-task', (e, args) => {
       console.log('create-task', args);
